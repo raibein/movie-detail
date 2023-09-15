d841cb8f6e0a82def726e51398cc15f9bcaf7758
Raben Shrestha <rabenshrestha@Rabens-Air.askey.com>
2023-09-15 17:10:18 -0500

    udpate readme and sh files

--- vars.tf
+++ vars.tf
@@ -15,7 +15,7 @@ variable "AMIS" {
   default = {
     # us-east-1 = "ami-06ca3ca175f37dd66"
     us-east-1 = "ami-06ca3ca175f37dd66"
-    nexus = "ami-002070d43b0a4f171"
+    nexus     = "ami-002070d43b0a4f171"
   }
 }
 
